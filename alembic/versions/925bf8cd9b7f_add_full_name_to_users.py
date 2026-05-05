f2ff056404d7213648e3d28314a83cb53f64c751
Ahmed Khairy <HNU.24.381.Ahmed-Ahmed@hnu.edu.eg>
2026-05-05 21:56:49 +0300

    feat: add full_name field to users table and update ignore patterns for Docker and Git

--- alembic/versions/925bf8cd9b7f_add_full_name_to_users.py
+++ alembic/versions/925bf8cd9b7f_add_full_name_to_users.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '925bf8cd9b7f'
-down_revision: Union[str, Sequence[str], None] = '83568857593b'
+down_revision: Union[str, Sequence[str], None] = '20260427_0001'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
